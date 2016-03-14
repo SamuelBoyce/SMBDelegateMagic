94e05f451ffb3e1579576b2fb831cf2a9544eac3
SamuelBoyce <samuel.boyce@gmail.com>
2016-03-14 13:10:07 -0400

    Making Magic

--- SMBDelegateMagic/ViewController.m
+++ SMBDelegateMagic/ViewController.m
@@ -7,21 +7,73 @@
 //
 
 #import "ViewController.h"
+#import "AppDelegate.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIPickerView *pickerView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (nonatomic, strong) NSArray *invocation;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.invocation = @[@"Per Adonai Eloim", @"Adonai Jehova", @"Adonai Sabaoth", @"Metraton On Agla Mathon", @"verbum pythonicum", @"mysterium salamandrae", @"conventus sylvorum", @"antra gnomorum", @"daemonia Coeli God", @"Almonsin", @"Gibor", @"Jehosua", @"Evam", @"Zariatnatmik", @"veni, veni, veni"];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    self.pickerView.delegate = self;
+    self.pickerView.dataSource = self;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.invocation.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    
+    cell = [self.tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    cell.textLabel.text = self.invocation[indexPath.row];
+    
+    return cell;
+}
+
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return self.invocation.count;
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    return self.invocation[row];
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self.pickerView selectRow:indexPath.row inComponent:0 animated:YES];
+    [self pickerView:self.pickerView didSelectRow:indexPath.row inComponent:0];
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    self.label.text = self.invocation[row];
 }
 
 @end
