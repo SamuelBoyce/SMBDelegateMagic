7a4c116f02ade4644c8bd09c77ed8fd294e9d1c9
SamuelBoyce <samuel.boyce@gmail.com>
2016-03-14 08:58:45 -0400

    Initial Commit

---
+++ SMBDelegateMagic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMBDelegateMagic
+//
+//  Created by Samuel Boyce on 3/14/16.
+//  Copyright © 2016 Samuel Boyce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMBDelegateMagic/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SMBDelegateMagic
+//
+//  Created by Samuel Boyce on 3/14/16.
+//  Copyright © 2016 Samuel Boyce. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
